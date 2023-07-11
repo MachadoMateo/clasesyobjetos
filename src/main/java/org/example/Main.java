ad60bb1b008b893cc0d763ccb4dd64e3a0c91c94
lntuberquia <natalia.tuberquia@udea.edu.co>
2023-07-11 11:43:23 -0500

    olo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.example.modelos.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+       int edad=26;
+       String nombre="Mateo";
+
+       Oferta  objetoOferta=new Oferta();
+       Local   objetoLocal= new Local();
+       Reserva objetoReserva= new Reserva();
+       Persona objetoPersona= new Persona();
+       Samurai objetoSamurai= new Samurai();
+        System.out.println(objetoSamurai.nombre);
+
+
+       Herramienta objetoHerramienta= new Herramienta();
+        System.out.println(objetoHerramienta.nombre);
+
+
+        Alimento objetoAlimento= new Alimento();
+        System.out.println(objetoAlimento.nombre);
+
+
+       //creando un objeto de la clase SAMURAI
+        //todo objeto es una VARIABLE
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/modelos/Persona.java
@@ -0,0 +1,13 @@
+package org.example.modelos;
+
+public class Persona {
+
+    public int id;
+    public String documento;
+    public String nombres;
+    public String correo;
+    public int ubicacion;
+
+    public Persona() {
+    }
+}
